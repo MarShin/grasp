5baf6640fa548534c50f06c8e7de08302c6be513
MartinShin <martinshin95@gmail.com>
2015-09-22 02:17:50 -0500

    commit design thinking project

---
+++ arduino_button/arduino_button.ino
@@ -0,0 +1,27 @@
+/*
+  DigitalReadSerial
+ Reads a digital input on pin 2, prints the result to the serial monitor 
+ 
+ This example code is in the public domain.
+ */
+
+// digital pin 2 has a pushbutton attached to it. Give it a name:
+const int recordButtonpin = 2;
+int buttonState=0;
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // make the pushbutton's pin an input:
+  pinMode(recordButtonpin, INPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  //while (Serial.available()==0);
+  // read the input pin:
+  buttonState = digitalRead(recordButtonpin);
+  // print out the state of the button:
+  Serial.print(buttonState);
+  delay(50);        // delay in between reads for stability
+}
---
+++ arduino_grip/arduino_grip.ino
@@ -0,0 +1,56 @@
+const int relay1=11;
+const int relay2=12;
+const int gripButtonPin = 2;
+int switchStat=0; //var for determining the state 0-stop 1-upward 2-downward 3-forbidden
+
+boolean firstRun=true;
+
+void setup() {
+  pinMode(relay1, OUTPUT);
+  pinMode(relay2, OUTPUT);
+
+  Serial.begin(9600);
+  pinMode(gripButtonPin, INPUT);
+  stopGrip();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  switchStat=digitalRead(gripButtonPin);
+  Serial.println(switchStat);
+  delay(10);
+  
+  if (switchStat==1) {
+      if(firstRun){
+      startGrip();
+      delay (550); //grip duration
+      firstRun=false;
+      }
+  }
+  stopGrip();
+  // delay (3000); //depends on duration of video
+  if (switchStat==0 && !firstRun){
+     releaseGrip();
+     delay (500); //release duration
+     firstRun=!firstRun;
+  }  
+}
+
+void stopGrip(){
+  digitalWrite(relay1,LOW);
+  digitalWrite(relay2,LOW);
+  Serial.println("both are low ");
+}
+
+void releaseGrip(){
+  digitalWrite(relay1,LOW);
+  digitalWrite(relay2,HIGH);
+  Serial.println("relay1 is low relay2 is high ");
+}
+
+void startGrip(){
+  digitalWrite(relay1,HIGH);
+  digitalWrite(relay2,LOW);
+  Serial.println("relay1 is high relay2 is low ");
+}
+
